3a1bdc118f4b9ef3832a05afcc4e3ed85c905be1
farhan <farhanfadhilah5@gmail.com>
2025-06-22 22:04:31 +0700

    chore: Update project configuration and dependencies
    - Added .nvmrc file to specify Node.js version 18.17.0.
    - Updated next.config.js to include experimental SWC transform settings.
    - Upgraded Next.js and ESLint configuration dependencies to version 13.5.11 in package.json and package-lock.json.
    - Introduced vercel.json for Vercel deployment configuration.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    forceSwcTransforms: false,
+  },
   i18n,
   images: {
     domains: [
